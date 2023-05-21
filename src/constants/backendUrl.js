6499732600acf9d12bf75805372fc1961ca7cb6e
aaejandroabele <alejandroabele@hotmail.com.ar>
2023-05-21 19:29:00 -0300

    feat: added filesByName services

--- src/constants/backendUrl.js
+++ src/constants/backendUrl.js
@@ -1,1 +1,2 @@
 export const filesPath = () => `/api/v1/files/data`;
+export const filesPathByName = (name) => `/api/v1/files/data/?fileName=${name}`;
--- src/services/files.js
+++ src/services/files.js
@@ -1,16 +1,35 @@
 import { get } from "../utils/fetch";
-import { filesPath } from "../constants/backendUrl";
+import { filesPath, filesPathByName } from "../constants/backendUrl";
 const getFilesService = async () => {
   let response = [];
   try {
     const res = await get(filesPath());
-    response = res?.payload;
-  } catch (error) {}
+    if (res && res.payload) {
+      response = res.payload
+    }
+  } catch (error) {
+    throw error
+
+  }
+  return response;
+};
+const getFilesByNameService = async (name) => {
+  let response = [];
+  try {
+    const res = await get(filesPathByName(name));
+    if (res && res.payload) {
+      response = res.payload
+    }
+  } catch (error) {
+    throw error
+  }
   return response;
 };
 
 
 
+
 export {
   getFilesService,
+  getFilesByNameService
 };

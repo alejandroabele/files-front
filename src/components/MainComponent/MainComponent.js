3821230c339e6c57ad52a949e7ec6150ee7e3900
aaejandroabele <alejandroabele@hotmail.com.ar>
2023-05-21 19:29:13 -0300

    feat: added filter by name search

--- src/components/MainComponent/MainComponent.js
+++ src/components/MainComponent/MainComponent.js
@@ -1,16 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Table from 'react-bootstrap/Table';
+import FormControl from 'react-bootstrap/FormControl';
+import useFiles from '../../hooks/useFiles'
 import TheadComponent from '../TheadComponent';
 import TbodyComponent from '../TbodyComponent';
-const headers = ['Filename', 'Text', 'Number', 'Hex'];
+import { useDispatch } from 'react-redux';
+import { setFiles } from '../../redux/actions';
 
-const MainComponent = ({ fileData }) => {
+
+
+const MainComponent = () => {
+    const [filterValue, setFilterValue] = useState('');
+    const { findFilesByName } = useFiles()
+    const [debouncedFilterValue, setDebouncedFilterValue] = useState('');
+
+    
+    const { getFiles } = useFiles()
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (debouncedFilterValue) {
+            findFilesByName(debouncedFilterValue).then(data => {
+                dispatch(setFiles(data))
+            }).catch(e => {
+                dispatch(setFiles(e))
+            })
+        } else {
+            getFiles().then(data => {
+                dispatch(setFiles(data))
+            }).catch(e => {
+                dispatch(setFiles(e))
+            })
+        }
+
+    }, [debouncedFilterValue]);
+
+
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setDebouncedFilterValue(filterValue);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [filterValue]);
+
+    const handleFilterChange = (event) => {
+        setFilterValue(event.target.value);
+    };
+
     return (
         <div>
-            
+            <FormControl
+                type="text"
+                value={filterValue}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por nombre"
+            />
             <Table striped bordered hover>
-                <TheadComponent headers={headers} />
-                <TbodyComponent fileData={fileData} />
+                <TheadComponent />
+                <TbodyComponent />
             </Table>
         </div>)
 }
